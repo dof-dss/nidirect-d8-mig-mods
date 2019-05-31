b474bb0ea4e1c2261d4b928770a91b0f98954b91
duttonm <martin@theduttons.org>
2019-05-31 15:49:22 +0100

    first working case of combined test

--- migrate_nidirect_node/migrate_nidirect_node_article/tests/src/Nightwatch/Tests/articleContentTypeMigration.js
+++ migrate_nidirect_node/migrate_nidirect_node_article/tests/src/Nightwatch/Tests/articleContentTypeMigration.js
@@ -3,9 +3,15 @@ module.exports = {
 
     'Test whether Article content type exists': browser => {
         browser
-            .drupalLogin({ name: 'admin', password: 'letmein' })
+            .drupalLogin({ name: 'admin', password: 'letmein' });
+
+        browser
             .drupalRelativeURL('/admin/structure/types')
             .expect.element('table > tbody > tr:nth-child(2) > td.menu-label')
             .text.to.equal('Article');
+
+        browser
+            .drupalRelativeURL('/admin/structure/types/manage/article/fields')
+            .expect.element('#field-banner-image').to.be.present;
     }
 };
