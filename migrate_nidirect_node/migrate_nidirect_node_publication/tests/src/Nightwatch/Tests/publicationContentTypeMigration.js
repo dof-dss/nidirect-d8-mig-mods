f2f513d5fa0536144a1853d75e80a6bd79356b7f
Johan Gant <johan.gant@gmail.com>
2019-06-03 14:58:24 +0100

    Refine functional tests
    Remove redundant calls for login/logout and page loads

--- migrate_nidirect_node/migrate_nidirect_node_publication/tests/src/Nightwatch/Tests/publicationContentTypeMigration.js
+++ migrate_nidirect_node/migrate_nidirect_node_publication/tests/src/Nightwatch/Tests/publicationContentTypeMigration.js
@@ -10,61 +10,35 @@ module.exports = {
   },
   'Test whether Publication content type field exists (field_top_level_theme)': browser => {
     browser
-      .drupalLogin({ name: 'admin', password: 'letmein' })
       .drupalRelativeURL('/admin/structure/types/manage/publication/fields')
       .expect.element('#field-top-level-theme').to.be.present;
   },
   'Test whether Publication content type field exists (field_subtheme)': browser => {
-    browser
-      .drupalLogin({ name: 'admin', password: 'letmein' })
-      .drupalRelativeURL('/admin/structure/types/manage/publication/fields')
-      .expect.element('#field-subtheme').to.be.present;
+    browser.expect.element('#field-subtheme').to.be.present;
   },
   'Test whether Publication content type field exists (field_site_topics)': browser => {
-    browser
-      .drupalLogin({ name: 'admin', password: 'letmein' })
-      .drupalRelativeURL('/admin/structure/types/manage/publication/fields')
-      .expect.element('#field-site-topics').to.be.present;
+    browser.expect.element('#field-site-topics').to.be.present;
   },
   'Test whether Publication content type field exists (field_meta_tags)': browser => {
-    browser
-      .drupalLogin({ name: 'admin', password: 'letmein' })
-      .drupalRelativeURL('/admin/structure/types/manage/publication/fields')
-      .expect.element('#field-meta-tags').to.be.present;
+    browser.expect.element('#field-meta-tags').to.be.present;
   },
   'Test whether Publication content type field exists (field_publication_type)': browser => {
-    browser
-      .drupalLogin({ name: 'admin', password: 'letmein' })
-      .drupalRelativeURL('/admin/structure/types/manage/publication/fields')
-      .expect.element('#field-publication-type').to.be.present;
+    browser.expect.element('#field-publication-type').to.be.present;
   },
   'Test whether Publication content type field exists (field_published_date)': browser => {
-    browser
-      .drupalLogin({ name: 'admin', password: 'letmein' })
-      .drupalRelativeURL('/admin/structure/types/manage/publication/fields')
-      .expect.element('#field-published-date').to.be.present;
+    browser.expect.element('#field-published-date').to.be.present;
   },
   'Test whether Publication content type field exists (field_summary)': browser => {
-    browser
-      .drupalLogin({ name: 'admin', password: 'letmein' })
-      .drupalRelativeURL('/admin/structure/types/manage/publication/fields')
-      .expect.element('#field-summary').to.be.present;
+    browser.expect.element('#field-summary').to.be.present;
   },
   'Test whether Publication content type field exists (body)': browser => {
-    browser
-      .drupalLogin({ name: 'admin', password: 'letmein' })
-      .drupalRelativeURL('/admin/structure/types/manage/publication/fields')
-      .expect.element('#body').to.be.present;
+    browser.expect.element('#body').to.be.present;
   },
   'Test whether Publication content type field exists (field_attachment)': browser => {
-    browser
-      .drupalLogin({ name: 'admin', password: 'letmein' })
-      .drupalRelativeURL('/admin/structure/types/manage/publication/fields')
-      .expect.element('#field-attachment').to.be.present;
+    browser.expect.element('#field-attachment').to.be.present;
   },
   'Test whether we have any migrated content for publication nodes': browser => {
     browser
-      .drupalLogin({ name: 'admin', password: 'letmein' })
       .drupalRelativeURL('/admin/content?type=publication')
       .expect.element('#views-form-content-page-1 > table > tbody > tr')
       .text.to.not.contain('No content available');
