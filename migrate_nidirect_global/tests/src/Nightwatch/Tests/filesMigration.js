b4b3acf73dfd271bf7cfe03b3195865d6f5d15bc
duttonm <martin@theduttons.org>
2019-06-04 10:51:39 +0100

    added image file test

--- migrate_nidirect_global/tests/src/Nightwatch/Tests/filesMigration.js
+++ migrate_nidirect_global/tests/src/Nightwatch/Tests/filesMigration.js
@@ -9,5 +9,10 @@ module.exports = {
             .drupalRelativeURL('/admin/content/files')
             .expect.element('table > tbody > tr > td:nth-child(2)')
             .to.be.present;
+
+        browser
+            .drupalRelativeURL('/admin/content/files?filename=&filemime=image%2Fjpeg&status=All')
+            .expect.element('table > tbody > tr > td:nth-child(2)')
+            .to.have.text.which.contains('image');
     }
 };
