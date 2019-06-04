9d35c945ad8accd2cf36b0a0535355bbc352f501
duttonm <martin@theduttons.org>
2019-06-04 16:23:31 +0100

    XPath selector working

--- migrate_nidirect_node/migrate_nidirect_node_landing_page/tests/src/Nightwatch/Tests/landingPageContentTypeMigration.js
+++ migrate_nidirect_node/migrate_nidirect_node_landing_page/tests/src/Nightwatch/Tests/landingPageContentTypeMigration.js
@@ -65,9 +65,12 @@ module.exports = {
         // Now test a random sample of actual nodes.
 
         // Extract title from old NIDirect page.
+
+        // .elements('css selector', '#main-area div div:nth-child(3) div h1'
+
         browser
             .url('https://www.nidirect.gov.uk/node/4005')
-            .elements('css selector', '#main-area div div:nth-child(3) div h1', function(result) {
+            .elements('xpath', '//h1[@class]', function(result) {
                 result.value.map(function(element, err) {
                     browser.elementIdAttribute(element.ELEMENT, 'innerText', function(res) {
                         // Check that the same title appears in D8 after migration.
