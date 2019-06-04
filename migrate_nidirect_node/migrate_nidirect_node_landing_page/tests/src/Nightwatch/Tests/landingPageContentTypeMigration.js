3857e2e8a8f1e56f917367cf92bc3e01d1ada170
duttonm <martin@theduttons.org>
2019-06-04 15:12:53 +0100

    check for landing page fields

--- migrate_nidirect_node/migrate_nidirect_node_landing_page/tests/src/Nightwatch/Tests/landingPageContentTypeMigration.js
+++ migrate_nidirect_node/migrate_nidirect_node_landing_page/tests/src/Nightwatch/Tests/landingPageContentTypeMigration.js
@@ -9,5 +9,48 @@ module.exports = {
             .drupalRelativeURL('/admin/structure/types/manage/landing_page')
             .expect.element('form > div > #edit-name')
             .value.to.contain('Landing page');
+    },
+
+    'Test whether Landing Page fields exist': browser => {
+
+        browser
+            .drupalRelativeURL('/admin/structure/types/manage/landing_page/fields')
+            .expect.element('#field-banner-image').to.be.present;
+
+        browser
+            .drupalRelativeURL('/admin/structure/types/manage/landing_page/fields')
+            .expect.element('#field-banner-image-overlay').to.be.present;
+
+        browser
+            .drupalRelativeURL('/admin/structure/types/manage/landing_page/fields')
+            .expect.element('#body').to.be.present;
+
+        browser
+            .drupalRelativeURL('/admin/structure/types/manage/landing_page/fields')
+            .expect.element('#field-enable-title').to.be.present;
+
+        browser
+            .drupalRelativeURL('/admin/structure/types/manage/landing_page/fields')
+            .expect.element('#field-meta-tags').to.be.present;
+
+        browser
+            .drupalRelativeURL('/admin/structure/types/manage/landing_page/fields')
+            .expect.element('#field-summary').to.be.present;
+
+        browser
+            .drupalRelativeURL('/admin/structure/types/manage/landing_page/fields')
+            .expect.element('#field-teaser').to.be.present;
+
+        browser
+            .drupalRelativeURL('/admin/structure/types/manage/landing_page/fields')
+            .expect.element('#field-subtheme').to.be.present;
+
+        browser
+            .drupalRelativeURL('/admin/structure/types/manage/landing_page/fields')
+            .expect.element('#field-top-level-theme').to.be.present;
+
+        browser
+            .drupalRelativeURL('/admin/structure/types/manage/landing_page/fields')
+            .expect.element('#field-summary').to.be.present;
     }
 };
