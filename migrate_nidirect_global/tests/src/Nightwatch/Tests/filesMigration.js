230aac186d14d707e8e52f1550f4ccca0c9c775c
duttonm <martin@theduttons.org>
2019-06-04 11:18:28 +0100

    test for different file types and for one specific file

--- migrate_nidirect_global/tests/src/Nightwatch/Tests/filesMigration.js
+++ migrate_nidirect_global/tests/src/Nightwatch/Tests/filesMigration.js
@@ -3,15 +3,42 @@ module.exports = {
 
     'Test whether files have been migrated': browser => {
         browser
-            .drupalLogin({ name: 'admin', password: 'letmein' });
+            .drupalLogin({name: 'admin', password: 'letmein'});
 
         browser
             .drupalRelativeURL('/admin/content/files')
             .expect.element('table > tbody > tr > td:nth-child(2)')
             .to.be.present;
+    },
+
+    'Test whether any image files have been migrated': browser => {
 
         browser
             .drupalRelativeURL('/admin/content/files?filename=&filemime=image%2Fjpeg&status=All')
+            .expect.element('table > tbody > tr > td:nth-child(2)')
+            .text.to.contain('image/jpeg');
+    },
+
+    'Test whether any PDF files have been migrated': browser => {
+
+        browser
+            .drupalRelativeURL('/admin/content/files?filename=&filemime=application%2Fpdf&status=All')
+            .expect.element('table > tbody > tr > td:nth-child(2)')
+            .text.to.contain('application/pdf');
+    },
+
+    'Test whether any MS Word files have been migrated': browser => {
+
+        browser
+            .drupalRelativeURL('/admin/content/files?filename=&filemime=application%2Fmsword&status=All')
+            .expect.element('table > tbody > tr > td:nth-child(2)')
+            .text.to.contain('application/msword');
+    },
+
+    'Test for specific image file': browser => {
+
+        browser
+            .drupalRelativeURL('/admin/content/files?filename=%22Schooner+Result+after+completion%22&filemime=image%2Fjpeg&status=All')
             .expect.element('table > tbody > tr > td:nth-child(2)')
             .text.to.contain('image/jpeg');
     }
