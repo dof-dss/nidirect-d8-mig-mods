23a5084d070ba52712e8e18d4d1edb217ee04de8
duttonm <martin@theduttons.org>
2019-06-05 15:33:56 +0100

    check metatag keyword

--- migrate_nidirect_node/tests/src/Nightwatch/Tests/metatagMigration.js
+++ migrate_nidirect_node/tests/src/Nightwatch/Tests/metatagMigration.js
@@ -15,6 +15,26 @@ module.exports = {
             .drupalRelativeURL('/admin/config/search/metatag')
             .useXpath()
             .expect.element('//td[text()="[node:field-summary]"]').to.be.present;
+    },
+
+    // Look for keyword 'exercise' on node 1130.
+    'Test whether individual custom metatags have been imported': browser => {
+        browser
+            .url('https://www.nidirect.gov.uk/node/1130')
+            .elements('xpath', "//meta[@name=\"keywords\"]", function(result) {
+                result.value.map(function(element, err) {
+                    browser.elementIdAttribute(element.ELEMENT, 'content', function(res) {
+                        // Check that the same title appears in D8 after migration.
+                        console.log(res.value);
+                        browser
+                            .drupalRelativeURL('/node/1130')
+                            .useXpath()
+                            .expect.element("//meta[@name=\"keywords\"]")
+                            .to.have.attribute('content')
+                            .which.equals(res.value);
+                    })
+                })
+            });
     }
 
 
