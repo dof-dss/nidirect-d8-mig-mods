51684afb4e50d185a9d24580a8017534feba5d70
Johan Gant <johan.gant@gmail.com>
2019-06-14 14:49:06 +0100

    Streamline test login step

--- migrate_nidirect_node/migrate_nidirect_node_article/tests/src/Nightwatch/Tests/articleContentTypeMigration.js
+++ migrate_nidirect_node/migrate_nidirect_node_article/tests/src/Nightwatch/Tests/articleContentTypeMigration.js
@@ -3,9 +3,7 @@ module.exports = {
 
     'Test whether Article content type exists': browser => {
         browser
-            .drupalLogin({ name: process.env.TEST_USER, password: process.env.TEST_PASS });
-
-        browser
+            .drupalLogin({ name: process.env.TEST_USER, password: process.env.TEST_PASS })
             .drupalRelativeURL('/admin/structure/types/manage/article')
             .expect.element('form > div > #edit-name')
             .value.to.contain('Article');
