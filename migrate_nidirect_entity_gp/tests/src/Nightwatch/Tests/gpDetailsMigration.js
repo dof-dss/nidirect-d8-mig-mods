e9d6378647a4466c338b1c1ca9f0e6fefe4f8eea
Johan Gant <johan.gant@gmail.com>
2019-06-14 12:05:42 +0100

    D7 GP details node content type > D8 GP custom content entity migration

---
+++ migrate_nidirect_entity_gp/tests/src/Nightwatch/Tests/gpDetailsMigration.js
@@ -0,0 +1,23 @@
+module.exports = {
+  '@tags': ['nidirect-migrations', 'nidirect-entity-gp'],
+
+  'Test whether GP entity exists': function (browser) {
+    browser
+      .drupalLogin({ name: 'admin', password: 'letmein' })
+      .drupalRelativeURL('/admin/structure/gp/settings')
+      // The text match takes a regex, not a literal string.
+      .expect.element('h1.page-title').text.to.match(/gp settings/i);
+  },
+  'Test whether base fields appear on the entity form page': function (browser) {
+    browser.drupalRelativeURL('/admin/structure/gp/settings/form-display');
+    browser.expect.element('#first-name').to.be.present;
+    browser.expect.element('#last-name').to.be.present;
+    browser.expect.element('#cypher').to.be.present;
+  },
+  'Test whether we have any migrated content for GP details': browser => {
+    browser
+      .drupalRelativeURL('/admin/content/gp')
+      .expect.element('table > tbody > tr')
+      .text.to.not.contain('No content available');
+  }
+};
