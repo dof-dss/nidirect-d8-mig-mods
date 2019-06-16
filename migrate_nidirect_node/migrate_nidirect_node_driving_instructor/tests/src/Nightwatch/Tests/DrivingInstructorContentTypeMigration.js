9e0196b9a5074f16afab910c068bb45b4fed17c7
Steve Cousins <me@stevecousins.co.uk>
2019-06-16 11:45:09 +0100

    D8NID-70 ~ Update driving instructor tests

--- migrate_nidirect_node/migrate_nidirect_node_driving_instructor/tests/src/Nightwatch/Tests/DrivingInstructorContentTypeMigration.js
+++ migrate_nidirect_node/migrate_nidirect_node_driving_instructor/tests/src/Nightwatch/Tests/DrivingInstructorContentTypeMigration.js
@@ -1,5 +1,8 @@
+var fs = require('fs');
+
 module.exports = {
     '@tags': ['nidirect-migrations'],
+
 
     'Test whether Driving Instructor content type exists': browser => {
         browser
@@ -16,41 +19,23 @@ module.exports = {
         browser
             .drupalRelativeURL('/admin/structure/types/manage/driving_instructor/fields')
             .expect.element('#field-di-adi-no').to.be.present;
-
         browser
-            .drupalRelativeURL('/admin/structure/types/manage/driving_instructor/fields')
             .expect.element('#field-di-areas').to.be.present;
-
         browser
-            .drupalRelativeURL('/admin/structure/types/manage/driving_instructor/fields')
             .expect.element('#field-di-categories').to.be.present;
-
         browser
-            .drupalRelativeURL('/admin/structure/types/manage/driving_instructor/fields')
             .expect.element('#field-email-address').to.be.present;
-
         browser
-            .drupalRelativeURL('/admin/structure/types/manage/driving_instructor/fields')
             .expect.element('#field-di-firstname').to.be.present;
-
         browser
-            .drupalRelativeURL('/admin/structure/types/manage/driving_instructor/fields')
             .expect.element('#field-di-lastname').to.be.present;
-
         browser
-            .drupalRelativeURL('/admin/structure/types/manage/driving_instructor/fields')
             .expect.element('#field-contact-phone').to.be.present;
-
         browser
-            .drupalRelativeURL('/admin/structure/types/manage/driving_instructor/fields')
             .expect.element('#field-meta-tags').to.be.present;
-
         browser
-            .drupalRelativeURL('/admin/structure/types/manage/driving_instructor/fields')
             .expect.element('#field-contact-sms').to.be.present;
-
         browser
-            .drupalRelativeURL('/admin/structure/types/manage/driving_instructor/fields')
             .expect.element('#field-link-url').to.be.present;
     },
 
@@ -62,51 +47,39 @@ module.exports = {
             .expect.element('#views-form-content-page-1 > table > tbody > tr > td:nth-child(3)')
             .text.to.contain('Driving instructor');
 
-        // Now test a random sample of actual nodes.
+        // Try loading a csv of nids for this content type to test against.
+        try {
+            fs.existsSync(__dirname + '/nids.csv')
 
-        // Extract title from old NIDirect page.
-        browser
-            .url('https://www.nidirect.gov.uk/node/11550')
-            .elements('xpath', '//*[@id="page-title"]', function (result) {
-                result.value.map(function (element, err) {
-                    browser.elementIdAttribute(element.ELEMENT, 'innerText', function (res) {
-                        // Check that the same title appears in D8 after migration.
-                        browser
-                            .drupalRelativeURL('/node/11550/edit')
-                            .expect.element('#edit-title-0-value')
-                            .to.have.value.which.contains(res.value);
-                    })
-                })
-            });
+            let nids_file = fs.readFileSync(__dirname + '/nids.csv', 'utf8');
+            var nids = JSON.parse("[" + nids_file + "]");
 
-        // Extract title from old NIDirect page.
-        browser
-            .url('https://www.nidirect.gov.uk/node/11622')
-            .elements('xpath', '//*[@id="page-title"]', function (result) {
-                result.value.map(function (element, err) {
-                    browser.elementIdAttribute(element.ELEMENT, 'innerText', function (res) {
-                        // Check that the same title appears in D8 after migration.
-                        browser
-                            .drupalRelativeURL('/node/11622/edit')
-                            .expect.element('#edit-title-0-value')
-                            .to.have.value.which.contains(res.value);
-                    })
-                })
-            });
+            if (nids.length > 0) {
+                // Shuffle the nids and select a sample.
+                nids = nids.sort(() => 0.5 - Math.random());
+                nids = nids.slice(0, 10);
 
-        // Extract title from old NIDirect page.
-        browser
-            .url('https://www.nidirect.gov.uk/node/11713')
-            .elements('xpath', '//*[@id="page-title"]', function (result) {
-                result.value.map(function (element, err) {
-                    browser.elementIdAttribute(element.ELEMENT, 'innerText', function (res) {
-                        // Check that the same title appears in D8 after migration.
-                        browser
-                            .drupalRelativeURL('/node/11713/edit')
-                            .expect.element('#edit-title-0-value')
-                            .to.have.value.which.contains(res.value);
-                    })
-                })
-            })
+                nids.forEach(nid => {
+                    browser
+                        .url('https://www.nidirect.gov.uk/node/' + nid)
+                        .elements('xpath', '//*[@id="page-title"]', function (result) {
+                            result.value.map(function (element, err) {
+                                browser.elementIdAttribute(element.ELEMENT, 'innerText', function (res) {
+                                    console.log(res.value)
+                                    // Check that the same title appears in D8 after migration.
+                                    browser
+                                        .drupalRelativeURL('/node/' + nid + '/edit')
+                                        .expect.element('#edit-title-0-value')
+                                        .to.have.value.which.contains(res.value);
+                                })
+                            })
+                        });
+                });
+            } else {
+                console.error('❗️Nids file found, but no nids parsed.')
+            }
+        } catch (err) {
+            console.error('❗️Nids file not found.')
+        }
     }
 };
