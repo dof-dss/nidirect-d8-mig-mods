e8255a097d991b82b6a37f1d876ca95df50e9a2c
Johan Gant <johan.gant@gmail.com>
2019-06-19 12:06:57 +0100

    Tweak to functional tests

--- migrate_nidirect_node/migrate_nidirect_node_external_link/tests/src/Nightwatch/Tests/externalLinkContentTypeMigration.js
+++ migrate_nidirect_node/migrate_nidirect_node_external_link/tests/src/Nightwatch/Tests/externalLinkContentTypeMigration.js
@@ -6,18 +6,19 @@ module.exports = {
       .drupalLogin({ name: process.env.TEST_USER, password: process.env.TEST_PASS })
       .drupalRelativeURL('/admin/structure/types/manage/external_link')
       // The text match takes a regex, not a literal string.
-      .expect.element('h1.external-link-title').text.to.match(/edit external link content type/i);
+      .expect.element('h1').text.to.match(/edit external link content type/i);
   },
   'Test whether External link content type fields exist': browser => {
     browser.drupalRelativeURL('/admin/structure/types/manage/external_link/fields');
-    // browser.expect.element('#field-enable-toc').to.be.present;
-    // browser.expect.element('#field-meta-tags').to.be.present;
-    // browser.expect.element('#body').to.be.present;
+    browser.expect.element('#field-link').to.be.present;
+    browser.expect.element('#field-site-themes').to.be.present;
+    browser.expect.element('#field-subtheme').to.be.present;
+    browser.expect.element('#field-top-level-theme').to.be.present;
   },
   'Test whether we have any migrated content for External link nodes': browser => {
     browser
       .drupalRelativeURL('/admin/content?type=external_link')
-      .expect.element('#views-form-content-external-link-1 > table > tbody > tr')
+      .expect.element('#views-form-content-page-1 > table > tbody > tr')
       .text.to.not.contain('No content available');
   }
 };
