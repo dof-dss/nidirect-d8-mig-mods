bab950b253d9538ed82b5b0d8ad6055450233afd
omahm <hello@onemanandhismouse.com>
2019-10-15 08:44:22 +0100

    D8NID-339 ~ Adding metatags process function

--- migrate_nidirect_node/src/NodeMigrationProcessors.php
+++ migrate_nidirect_node/src/NodeMigrationProcessors.php
@@ -85,4 +85,62 @@ class NodeMigrationProcessors {
 
   }
 
+  public function metatags() {
+    $updated = 0;
+    $failed_updates = [];
+
+    // Verify that the metatag module is enabled.
+    $moduleHandler = \Drupal::service('module_handler');
+    if (!$moduleHandler->moduleExists('metatag')) {
+      return;
+    }
+
+    // Verify Drupal 7 metatag table exists.
+    if (!$this->dbConnMigrate->schema()->tableExists('metatag')) {
+      return;
+    }
+
+    // Get a list of custom metatags from NIDirect (D7)
+    // (only take the latest revision).
+    $query = $this->dbConnMigrate->query("select m1.entity_id, m1.data 
+        from {metatag} m1
+        join (select max(revision_id) as revision_id, entity_id
+              from {metatag}
+              where data like 'a:1:_s:8:%'
+              and entity_type = 'node' group by entity_id) m2
+        on m1.entity_id = m2.entity_id
+        and m1.revision_id = m2.revision_id");
+    $results = $query->fetchAllKeyed();
+
+    // Loop through and update nodes in NIDirect (D8).
+    foreach ($results as $entity_id => $data) {
+      $new_data = unserialize($data);
+      if (isset($new_data['keywords']) || isset($new_data['abstract'])) {
+        $key = 'keywords';
+        if (isset($new_data['abstract'])) {
+          $key = 'abstract';
+        }
+        $value = $new_data[$key]['value'];
+        // Load the node in D8.
+        $node = Node::load($entity_id);
+        if ($node) {
+          // Retrieve the existing metatags.
+          $meta = unserialize(($node->field_meta_tags->value));
+          // Set the keyword/abstract.
+          $meta[$key] = $value;
+          // Save the node.
+          $node->field_meta_tags->value = serialize($meta);
+          $node->save();
+          $updated++;
+        }
+        else {
+          $failed_updates[] = $entity_id;
+        }
+      }
+      else {
+        // If it isn't 'abstract' or 'keyword' then fail it.
+        $failed_updates[] = $entity_id;
+      }
+    }
+  }
 }
