a7557ba29ee6dc9dcba20897d16eb580a9634ff3
omahm <hello@onemanandhismouse.com>
2019-11-06 10:57:51 +0000

    D8NID-322 ~ Perform query against lookup table for problematic telephone values

---
+++ migrate_nidirect_node/src/Plugin/migrate/process/TelephonePlus.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Drupal\migrate_nidirect_node\Plugin\migrate\process;
+
+use Drupal\Core\Database\Database;
+use Drupal\migrate\ProcessPluginBase;
+use Drupal\migrate\MigrateExecutableInterface;
+use Drupal\migrate\Row;
+
+/**
+ * Transforms string based phone field to TelephonePlus Plus field.
+ *
+ * Takes a string value and processes for compatibility with TelephonePlus Plus by:
+ * - Preform a lookup against a table of known values that cannot be
+ *   automatically imported and use the defined replacement value.
+ * - Pass the value through a series of regex statements to return a
+ *   suitable value for import.
+ *
+ * Examples:
+ *
+ * @MigrateProcessPlugin(
+ *  id = "telephone_plus"
+ * )
+ */
+class TelephonePlus extends ProcessPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property)
+  {
+    $source_id = $row->getSourceIdValues();
+    $nid = $source_id['nid'];
+
+    // Checkup lookup table for problematic phone numbers;
+    $db = Database::getConnection('default', 'default');
+
+    $result = $db->query(
+      'SELECT id, telephone_source, telephone_destination FROM {telephone_migration_lookup} t WHERE t.id = :id',
+      [':id' => $nid]);
+
+    if (is_array($data = $result->fetchAssoc())) {
+      var_dump($data);
+      return;
+    }
+
+  }
+
+}
