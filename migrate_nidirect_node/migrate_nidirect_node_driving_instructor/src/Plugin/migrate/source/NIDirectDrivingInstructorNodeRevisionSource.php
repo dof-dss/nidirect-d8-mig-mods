c8609557dc7e2c3213a294a4627b771bbe9f2a36
omahm <hello@onemanandhismouse.com>
2019-11-15 09:30:55 +0000

    D8NID-322 ~ Update Driving Instructor revisions to use telephone field

---
+++ migrate_nidirect_node/migrate_nidirect_node_driving_instructor/src/Plugin/migrate/source/NIDirectDrivingInstructorNodeRevisionSource.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Drupal\migrate_nidirect_node_driving_instructor\Plugin\migrate\source;
+
+use Drupal\node\Plugin\migrate\source\d7\NodeRevision;
+use Drupal\migrate\Row;
+
+/**
+ * Prepares Driving instructor nodes for telephone plus field.
+ *
+ * @MigrateSource(
+ *   id = "driving_instructor_node_revision_source",
+ * )
+ */
+class NIDirectDrivingInstructorNodeRevisionSource extends NodeRevision {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function prepareRow(Row $row) {
+    $telephone = [];
+    $ids = $row->getSourceIdValues();
+
+    // Fetch landline phone number.
+    $query = $this->getDatabase()->query('
+      SELECT field_contact_phone_value
+      FROM {field_revision_field_contact_phone}
+      WHERE revision_id = :vid', [
+        ':vid' => $vid,
+      ]
+    );
+
+    $landline = $query->fetchField();
+
+    if (!empty($landline)) {
+      $telephone[] = [
+        'telephone_title' => 'Landline',
+        'telephone_number' => $landline,
+        'telephone_extension' => '',
+        'telephone_supplementary' => '',
+        'country_code' => 'GB',
+        'display_international_number' => '0',
+      ];
+    }
+
+    // Fetch mobile phone number.
+    $query = $this->getDatabase()->query('
+      SELECT field_contact_sms_value
+      FROM {field_revision_field_contact_sms}
+      WHERE revision_id = :vid', [
+        ':vid' => $vid,
+      ]
+    );
+
+    $mobile = $query->fetchField();
+
+    if (!empty($mobile)) {
+      $telephone[] = [
+        'telephone_title' => 'Mobile',
+        'telephone_number' => $mobile,
+        'telephone_extension' => '',
+        'telephone_supplementary' => '',
+        'country_code' => 'GB',
+        'display_international_number' => '0',
+      ];
+    }
+
+    $row->setSourceProperty('telephone_number', $telephone);
+    return parent::prepareRow($row);
+  }
+
+}
