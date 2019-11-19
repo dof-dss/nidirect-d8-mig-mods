2e63ae30bee43e1d4c736802cfc5f3e861b92521
omahm <hello@onemanandhismouse.com>
2019-11-19 12:10:35 +0000

    D8NID-322 ~ Added generic node and revision source plugins for entities with ingle phone fields

---
+++ migrate_nidirect_node/src/Plugin/migrate/source/NIDirectPhoneFieldRevisionNodeSource.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Drupal\migrate_nidirect_node\Plugin\migrate\source;
+
+use Drupal\migrate_nidirect_utils\TelephonePlusUtils;
+use Drupal\migrate\Row;
+use Drupal\node\Plugin\migrate\source\d7\NodeRevision;
+
+/**
+ * Prepares revisions with phone fields for conversion to telephone plus field.
+ *
+ * @MigrateSource(
+ *   id = "phone_field_revision_node_source",
+ * )
+ */
+class NIDirectPhoneFieldRevisionNodeSource extends NodeRevision {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function prepareRow(Row $row) {
+    $ids = $row->getSourceIdValues();
+
+    // Fetch phone number.
+    $query = $this->getDatabase()->query('
+      SELECT field_contact_phone_value
+      FROM {field_revision_field_contact_phone}
+      WHERE revision_id = :vid', [
+        ':vid' => $ids['vid'],
+      ]
+    );
+
+    $phone = $query->fetchField();
+    $telephone = TelephonePlusUtils::parse($phone);
+
+    $row->setSourceProperty('telephone_number', $telephone);
+    return parent::prepareRow($row);
+  }
+
+}
