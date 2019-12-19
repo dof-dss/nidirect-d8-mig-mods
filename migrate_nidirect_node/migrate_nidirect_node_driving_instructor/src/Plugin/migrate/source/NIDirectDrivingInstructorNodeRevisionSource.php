ed3d7e8ad27436d21d829b999873af34d1d34c1a
Johan Gant <johan.gant@gmail.com>
2019-12-19 13:45:18 +0000

    Add extra source_module annotations to plugins

--- migrate_nidirect_node/migrate_nidirect_node_driving_instructor/src/Plugin/migrate/source/NIDirectDrivingInstructorNodeRevisionSource.php
+++ migrate_nidirect_node/migrate_nidirect_node_driving_instructor/src/Plugin/migrate/source/NIDirectDrivingInstructorNodeRevisionSource.php
@@ -10,6 +10,7 @@ use Drupal\migrate\Row;
  *
  * @MigrateSource(
  *   id = "driving_instructor_node_revision_source",
+ *   source_module = "migrate_nidirect_node_driving_instructor"
  * )
  */
 class NIDirectDrivingInstructorNodeRevisionSource extends NodeRevision {
