a7b6e1a0bfd0c56624c683ee92557643decd2298
DuttonMa <martin@theduttons.org>
2020-01-08 11:14:51 +0000

    nw approach working without DI

--- migrate_nidirect_utils/src/Command/NidirectMigratePostAliasCommand.php
+++ migrate_nidirect_utils/src/Command/NidirectMigratePostAliasCommand.php
@@ -41,68 +41,34 @@ class NidirectMigratePostAliasCommand extends ContainerAwareCommand {
     $conn_drupal8 = Database::getConnection('default', 'default');
     $this->getIo()->info('Started post migration alias processing.');
 
-    // Verify Drupal 7 flag table exists.
-    if (!$conn_migrate->schema()->tableExists('url_alias')) {
-      return 3;
-    }
+    // Retrieve all aliases from D7.
+    $query = $conn_migrate->query(
+      "select source, alias from {url_alias}");
+    $d7_aliases = $query->fetchAll();
+    foreach ($d7_aliases as $d7_alias) {
+      $d7_path = $d7_alias->source;
+      $d7_alias = $d7_alias->alias;
 
-    // Select aliases of interest from D8.
-    $query = $conn_drupal8->query(
-      "select path, alias from {path_alias} where alias like '%-to-%'  and alias not like '/articles%'"
-    );
-    $alias_list = $query->fetchAll();
-    $strings_removed = ['a', 'an', 'as', 'at', 'before', 'but', 'for', 'from', 'is',
-      'in', 'into', 'like', 'of', 'off', 'on', 'onto', 'per', 'since', 'than', 'the',
-      'this', 'that', 'to', 'up', 'via', 'with'];
-    foreach ($alias_list as $thisalias) {
-      $this_path = $thisalias->path;
-      $this_path_d7 = substr($this_path, 1);
-      $full_alias = $thisalias->alias;
-      // Derive shortened alias (with keywords removed).
-      $short_alias = $full_alias;
-      foreach ($strings_removed as $keyword) {
-        $short_alias = preg_replace('/-' . $keyword . '-/', '-', $short_alias);
-      }
-      if ($short_alias == $full_alias) {
-        continue;
-      }
-      $short_alias_d7 = substr($short_alias, 1);
-      $this->getIo()->info(
-        'Looking for alias on D7 - ' . $short_alias_d7 . ', and source - ' . $this_path_d7
-      );
+      // On D8, aliases and paths are prefixed with '/'.
+      $d8_alias = '/' . $d7_alias;
+      $d8_path = '/' . $d7_path;
 
-      // See if shortened alias existed on D7 for this item.
-      $query = $conn_migrate->query(
-        "select * from {url_alias} where alias = :short_alias and source = :this_path",
-        [':short_alias' => $short_alias_d7, ':this_path' => $this_path_d7]);
-      $d7_check = $query->fetchAll();
-      if (count($d7_check) == 0) {
-        // Shortened alias did not exist on D7, no further action required.
-        $this->getIo()->info(
-          'does not exist on D7'
-        );
-        continue;
-      }
-
-      // See if shortened alias already exists on D8 for this item.
+      // See if this alias exists on D8.
       $query = $conn_drupal8->query(
-        "select * from {path_alias} where alias = :short_alias and path = :this_path",
-        [':short_alias' => $short_alias, ':this_path' => $this_path]);
+        "select * from {path_alias} where alias = :d8_alias",
+        [':d8_alias' => $d8_alias]);
       $d8_check = $query->fetchAll();
       if (count($d8_check) > 0) {
-        // Shortened alias already exists on D8, no further action required.
-        $this->getIo()->info(
-          'already exists on D8'
-        );
+        // Alias already exists on D8, no further action required.
         continue;
       }
 
       // If we get to here then we need to create the shortened alias in D8.
       //$aliasManager = $this->container->get('path_alias.manager');
       //Need to use the DI service for this !!
-      \Drupal::service('path.alias_storage')->save($this_path, $short_alias);
+      \Drupal::service('path.alias_storage')->save($d8_path, $d8_alias);
       $this->getIo()->info(
-        '** Creating alias - ' . $short_alias
+        '** Creating alias - ' . $d8_alias
       );
       //$this->pathAliasManager->save($this_path, $short_alias);
     }
