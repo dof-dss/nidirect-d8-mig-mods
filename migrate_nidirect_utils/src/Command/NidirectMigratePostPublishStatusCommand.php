568ca1769cab0dca69eea83777a941e1cf778bd8
DuttonMa <martin@theduttons.org>
2020-03-05 15:27:07 +0000

    refactoring

--- migrate_nidirect_utils/src/Command/NidirectMigratePostPublishStatusCommand.php
+++ migrate_nidirect_utils/src/Command/NidirectMigratePostPublishStatusCommand.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\migrate_nidirect_utils\Command;
 
+use Drupal\Core\Database\Connection;
 use Drupal\node\Entity\Node;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -49,61 +50,9 @@ class NidirectMigratePostPublishStatusCommand extends ContainerAwareCommand {
     ", [':nids[]' => array_keys($d8_nids)]);
     $migrate_nid_status = $query->fetchAll();
 
-    // Sync our D8 node publish values with those from D7.
-    // There are three tables that need an adjustment ranging
-    // from node revisions to content moderation tracking tables.
+    // Sync our D8 node publish values and revisions with those from D7.
     foreach ($migrate_nid_status as $row) {
-      // Need to fetch the D8 revision ID for any node as it doesn't always match the source db.
-      $d8_vid = $conn_drupal8->query(
-        "SELECT vid FROM {node_field_data} WHERE nid = :nid", [':nid' => $row->nid]
-        )->fetchField();
-
-      // Run an update statement per item. Refinement might be to run a cross-DB SELECT query to power an UPDATE using a JOIN.
-      $query = $conn_drupal8->update('node_field_data')
-        ->fields(['status' => $row->status])
-        ->condition('nid', $row->nid)
-        ->execute();
-
-      $query = $conn_drupal8->update('node_field_revision')
-        ->fields(['status' => $row->status])
-        ->condition('nid', $row->nid)
-        ->condition('vid', $d8_vid)
-        ->execute();
-
-      // Get the D7 revision id.
-      $vid = $conn_migrate->query(
-        "SELECT vid FROM {node} WHERE nid = :nid", [':nid' => $row->nid]
-      )->fetchField();
-
-      // Update the current revision if necessary.
-      if ($vid != $d8_vid) {
-        $vid = $this->updateCurrentRevision($conn_migrate, $conn_drupal8, $row->nid, $vid, $d8_vid);
-      }
-
-      // The 'revision_translation_affected' field is poorly documented (and
-      // understood) in Drupal core, and is sometimes set to NULL after migrating
-      // content from Drupal 7. There is much discussion at
-      // https://www.drupal.org/project/drupal/issues/2746541 but after testing
-      // and investigation I am yet to find a case where it should not be set to '1'.
-      // Hence, we set it to '1' across the board to solve the problem of revisions
-      // not appearing on the revisions tab.
-      $query = $conn_drupal8->update('node_field_revision')
-        ->fields(['revision_translation_affected' => 1])
-        ->condition('nid', $row->nid)
-        ->execute();
-
-      $query = $conn_drupal8->update('content_moderation_state_field_data')
-        ->fields(['moderation_state' => 'published'])
-        ->condition('content_entity_id', $row->nid)
-        ->condition('content_entity_revision_id', $vid)
-        ->execute();
-
-      // Make sure that we have a 'published' revision.
-      $query = $conn_drupal8->update('content_moderation_state_field_revision')
-        ->fields(['moderation_state' => 'published'])
-        ->condition('content_entity_id', $row->nid)
-        ->condition('content_entity_revision_id', $vid)
-        ->execute();
+      $this->processNodeStatus($conn_migrate, $conn_drupal8, $row->nid, $row->status);
     }
 
     $this->getIo()->info('Updated revisions on ' . count($migrate_nid_status) . ' nodes.');
@@ -112,8 +61,79 @@ class NidirectMigratePostPublishStatusCommand extends ContainerAwareCommand {
   }
 
   /**
+   * Updates the status and revisions for the specified node.
+   *
+   * @param Drupal\Core\Database\Connection $conn_migrate
+   *   Drupal 7 database connection.
+   * @param Drupal\Core\Database\Connection $conn_drupal8
+   *   Drupal 8 database connection.
+   * @param int $nid
+   *   The node id.
+   * @param string $status
+   *   The status of the node.
+   *
+   */
+  private function processNodeStatus(Connection $conn_migrate, Connection $conn_drupal8, int $nid, string $status) {
+    // Need to fetch the D8 revision ID for any node as it doesn't always match the source db.
+    $d8_vid = $conn_drupal8->query(
+      "SELECT vid FROM {node_field_data} WHERE nid = :nid", [':nid' => $nid]
+    )->fetchField();
+
+    // Run an update statement per item. Refinement might be to run a cross-DB SELECT query to power an UPDATE using a JOIN.
+    $query = $conn_drupal8->update('node_field_data')
+      ->fields(['status' => $status])
+      ->condition('nid', $nid)
+      ->execute();
+
+    $query = $conn_drupal8->update('node_field_revision')
+      ->fields(['status' => $status])
+      ->condition('nid', $nid)
+      ->condition('vid', $d8_vid)
+      ->execute();
+
+    // Get the D7 revision id.
+    $vid = $conn_migrate->query(
+      "SELECT vid FROM {node} WHERE nid = :nid", [':nid' => $nid]
+    )->fetchField();
+
+    // Update the current revision if necessary.
+    if ($vid != $d8_vid) {
+      $vid = $this->updateCurrentRevision($conn_migrate, $conn_drupal8, $nid, $vid, $d8_vid);
+    }
+
+    // The 'revision_translation_affected' field is poorly documented (and
+    // understood) in Drupal core, and is sometimes set to NULL after migrating
+    // content from Drupal 7. There is much discussion at
+    // https://www.drupal.org/project/drupal/issues/2746541 but after testing
+    // and investigation I am yet to find a case where it should not be set to '1'.
+    // Hence, we set it to '1' across the board to solve the problem of revisions
+    // not appearing on the revisions tab.
+    $query = $conn_drupal8->update('node_field_revision')
+      ->fields(['revision_translation_affected' => 1])
+      ->condition('nid', $nid)
+      ->execute();
+
+    $query = $conn_drupal8->update('content_moderation_state_field_data')
+      ->fields(['moderation_state' => 'published'])
+      ->condition('content_entity_id', $nid)
+      ->condition('content_entity_revision_id', $vid)
+      ->execute();
+
+    // Make sure that we have a 'published' revision.
+    $query = $conn_drupal8->update('content_moderation_state_field_revision')
+      ->fields(['moderation_state' => 'published'])
+      ->condition('content_entity_id', $nid)
+      ->condition('content_entity_revision_id', $vid)
+      ->execute();
+  }
+
+  /**
    * Updates the current revision for the given node.
    *
+   * @param Drupal\Core\Database\Connection $conn_migrate
+   *   Drupal 7 database connection.
+   * @param Drupal\Core\Database\Connection $conn_drupal8
+   *   Drupal 8 database connection.
    * @param int $nid
    *   The node id.
    * @param int $vid
@@ -124,7 +144,7 @@ class NidirectMigratePostPublishStatusCommand extends ContainerAwareCommand {
    * @return string
    *   Current revision id.
    */
-  private function updateCurrentRevision($conn_migrate, $conn_drupal8, int $nid, int $vid, int $d8_vid) {
+  private function updateCurrentRevision(Connection $conn_migrate, Connection $conn_drupal8, int $nid, int $vid, int $d8_vid) {
     // Does this revision exist in D8 ?
     $check_vid = $conn_drupal8->query(
       "SELECT vid FROM {node_field_revision} WHERE nid = :nid AND vid = :vid", [':nid' => $nid, ':vid' => $vid]
