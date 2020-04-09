ebbc7f1d47a22572497a22d2578d69b9e9974688
omahm <hello@onemanandhismouse.com>
2020-04-09 17:12:06 +0100

    D8NID-551 ~ Related links extractor class

---
+++ migrate_nidirect_utils/src/ContentExtractors.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Drupal\migrate_nidirect_utils;
+
+/**
+ * Class ContentExtractors.
+ *
+ * @package Drupal\migrate_nidirect_utils
+ */
+class ContentExtractors {
+
+  public static function relatedLinks($content) {
+
+    // Extract everything after the 'More useful links'.
+    $links_markup = substr($content[0][0]['value'], strpos($content[0][0]['value'], '<h2>More useful links</h2>' ) + 26);
+
+    // Extract uri and text.
+    $link_regex = '/<a href="(\S+?)".*?>\s*([^<]*)<\//m';
+    preg_match_all($link_regex, $links_markup, $matches, PREG_SET_ORDER, 0);
+
+    $links = [];
+
+    foreach($matches as $link) {
+      $links[] = [
+        'uri' => $link[1],
+        'title' => $link[2],
+        'options' => '',
+      ];
+    }
+
+    return $links;
+  }
+}
