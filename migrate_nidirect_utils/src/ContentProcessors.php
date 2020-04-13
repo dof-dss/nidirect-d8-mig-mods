cf66ef610b8ca98ca29981bc976ba5f967875b2e
omahm <hello@onemanandhismouse.com>
2020-04-13 17:28:58 +0100

    D8NID-551 ~ Code standards fixes

--- migrate_nidirect_utils/src/ContentProcessors.php
+++ migrate_nidirect_utils/src/ContentProcessors.php
@@ -9,24 +9,34 @@ namespace Drupal\migrate_nidirect_utils;
  */
 class ContentProcessors {
 
-  public static function relatedLinks($content) {
+  /**
+   * Extracts link elements from html after the string 'More useful links'.
+   *
+   * @param array $content
+   *   Drupal content field array.
+   *
+   * @return array
+   *   Array of Drupal Link field values..
+   */
+  public static function relatedLinks(array $content) {
 
     // Find the character index of the Useful links heading.
     $links_header_position = strpos($content[0][0]['value'], 'More useful links');
 
     if ($links_header_position === FALSE) {
-      return;
+      return [];
     }
 
     // Extract everything after the 'More useful links'.
-    $links_markup = substr($content[0][0]['value'],  $links_header_position);
+    $links_markup = substr($content[0][0]['value'], $links_header_position);
     // Extract uri and text.
     $link_regex = '/<a href="(\S+?)".*?>\s*([^<]*)<\//m';
     preg_match_all($link_regex, $links_markup, $matches, PREG_SET_ORDER, 0);
 
     $links = [];
 
-    foreach($matches as $link) {
+    // Create a Drupal link field entry for each extracted HTML link element.
+    foreach ($matches as $link) {
       $links[] = [
         'uri' => (strpos($link[1], '/') === 0 ? 'internal:' . $link[1] : $link[1]),
         'title' => $link[2],
