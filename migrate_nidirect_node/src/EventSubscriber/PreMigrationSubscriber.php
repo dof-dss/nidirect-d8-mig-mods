4c9585c2c132486d6d104251b14bc9ddd55afe7b
omahm <hello@onemanandhismouse.com>
2020-06-05 11:34:22 +0100

    D8NID-725 ~ Initial pre migrate event handler for nodes

---
+++ migrate_nidirect_node/src/EventSubscriber/PreMigrationSubscriber.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Drupal\migrate_nidirect_node\EventSubscriber;
+
+use Drupal\migrate\Event\MigrateEvents;
+use Drupal\migrate\Event\MigrateImportEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Drupal\Core\Logger\LoggerChannelFactory;
+use Drupal\migrate_nidirect_utils\MigrationProcessors;
+
+/**
+ * Class PreMigrationSubscriber.
+ *
+ * Post Migrate processes.
+ */
+class PreMigrationSubscriber implements EventSubscriberInterface {
+
+  /**
+   * Drupal\Core\Logger\LoggerChannelFactory definition.
+   *
+   * @var \Drupal\Core\Logger\LoggerChannelFactory
+   */
+  protected $logger;
+
+  /**
+   * NodeMigrationProcessors definition.
+   *
+   * @var \Drupal\migrate_nidirect_utils\MigrationProcessors
+   */
+  protected $migrationProcessors;
+
+  /**
+   * PostMigrationSubscriber constructor.
+   *
+   * @param \Drupal\Core\Logger\LoggerChannelFactory $logger
+   *   Drupal logger.
+   * @param \Drupal\migrate_nidirect_utils\MigrationProcessors $migration_processors
+   *   Migration processors.
+   */
+  public function __construct(LoggerChannelFactory $logger, MigrationProcessors $migration_processors) {
+    $this->logger = $logger->get('migrate_nidirect_node');
+    $this->migrationProcessors = $migration_processors;
+  }
+
+  /**
+   * Get subscribed events.
+   *
+   * @inheritdoc
+   */
+  public static function getSubscribedEvents() {
+    $events[MigrateEvents::PRE_IMPORT][] = ['onMigratePreImport'];
+    return $events;
+  }
+
+  /**
+   * Handle post import migration event.
+   *
+   * @param \Drupal\migrate\Event\MigrateImportEvent $event
+   *   The import event object.
+   */
+  public function onMigratePreImport(MigrateImportEvent $event) {
+    $event_id = $event->getMigration()->getBaseId();
+
+    // Only process nodes, nothing else.
+    if (substr($event_id, 0, 5) == 'node_') {
+
+    }
+  }
+
+}
