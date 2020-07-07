25c9eddc3770f2e9ec279ac07cf9da2a34585cf0
omahm <hello@onemanandhismouse.com>
2020-07-07 16:01:49 +0100

    mend

--- migrate_nidirect_utils/src/Command/NidirectMigratePostNodeLanguageCommand.php
+++ migrate_nidirect_utils/src/Command/NidirectMigratePostNodeLanguageCommand.php
@@ -6,6 +6,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Drupal\Console\Core\Command\ContainerAwareCommand;
 use Drupal\Console\Annotations\DrupalCommand;
+use Symfony\Component\Yaml\Yaml;
 
 /**
  * Class NidirectMigratePostNodeLanguageCommand.
@@ -29,7 +30,10 @@ class NidirectMigratePostNodeLanguageCommand extends ContainerAwareCommand {
    * {@inheritdoc}
    */
   protected function execute(InputInterface $input, OutputInterface $output) {
-
+    $file_path = drupal_get_path('module', 'migrate_nidirect_utils') . '/data/node.langcodes.yml';
+    $file_contents = file_get_contents($file_path);
+    $ymldata = Yaml::parse($file_contents);
+    var_dump($ymldata);
   }
 
 }
