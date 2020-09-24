2c1752a9578e09c3973ca582faa251de2429c1b7
Johan Gant <johan.gant@gmail.com>
2020-09-24 11:27:00 +0100

    D8NID-850 Add drupal console command to clear existing feature/fcl nodes (#63)
    * Add drupal console command to clear existing feature/fcl nodes
    
    * Add post migrate command
    
    * Coding standards fixes
    
    * Create feature and FCL nodes
    
    * phpcs adjustments
    
    * Adjust target id

---
+++ migrate_nidirect_utils/src/Command/NidirectMigratePostFeatureNodesCommand.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace Drupal\migrate_nidirect_utils\Command;
+
+use Drupal\Console\Core\Command\ContainerAwareCommand;
+// @codingStandardsIgnoreStart
+use Drupal\Console\Annotations\DrupalCommand;
+use Drupal\node\Entity\Node;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+// @codingStandardsIgnoreEnd
+/**
+ * Recreates known feature and featured_content_list nodes after migration.
+ *
+ * @DrupalCommand (
+ *     extension="migrate_nidirect_utils",
+ *     extensionType="module"
+ * )
+ */
+class NidirectMigratePostFeatureNodesCommand extends ContainerAwareCommand {
+
+  /**
+   * A collection of featured content data.
+   * @var array
+   */
+  protected $featureContent = [];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function configure() {
+    $this->setName('nidirect:migrate:post:feature_nodes')
+      ->setDescription("Post migration: Recreates feature + featured_content_list nodes after migration.");
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function execute(InputInterface $input, OutputInterface $output) {
+    $this->task_create_feature_nodes();
+    $this->task_create_feature_content_list_nodes();
+
+    $this->getIo()->info('DONE!');
+  }
+
+  /**
+   * Re-create feature nodes from defined content.
+   */
+  // phpcs:disable
+  protected function task_create_feature_nodes() {
+  // phpcs:enable
+    $this->featureContent[] = [
+      'title' => 'Wear a face covering to help reduce spread of COVID-19',
+      'teaser' => 'Wear a face covering to help reduce spread of COVID-19 - they are now mandatory in certain indoor settings',
+      'uri' => 'internal:/node/13662',
+      'media_id' => 8939,
+    ];
+    $this->featureContent[] = [
+      'title' => 'Coronavirus (COVID-19)',
+      'teaser' => 'Updates and advice about coronavirus (COVID-19), including information about government services',
+      'uri' => 'internal:/node/13394',
+      'media_id' => 8786,
+    ];
+    $this->featureContent[] = [
+      'title' => 'Universal Credit',
+      'teaser' => 'Find out all you need to need to know to make a Universal Credit claim',
+      'uri' => 'internal:/node/12849',
+      'media_id' => 7283,
+    ];
+
+    foreach ($this->featureContent as &$feature) {
+      $node = Node::create([
+        'type' => 'feature',
+        'langcode' => 'en',
+        'moderation_state' => 'published',
+        'status' => 1,
+        'uid' => 1,
+        'title' => $feature['title'],
+        'field_teaser' => $feature['teaser'],
+        'field_link_url' => [
+          'uri' => $feature['uri'],
+        ],
+        'field_photo' => [
+          'target_id' => $feature['media_id'],
+        ],
+      ]);
+      $node->save();
+      $feature['nid'] = $node->id();
+
+      $this->getIo()->info("Created feature node with title '" . $feature['title'] . "'");
+    }
+  }
+
+  /**
+   * Re-create featured content list nodes from defined content.
+   */
+  // phpcs:disable
+  protected function task_create_feature_content_list_nodes() {
+  // phpcs:enable
+    $fcl_content[] = [
+      'title' => 'News: featured content',
+      'features' => [
+        7366,
+        7479,
+      ],
+      'tag' => 1344,
+    ];
+    $fcl_content[] = [
+      'title' => 'Homepage: featured content',
+      'features' => [
+        ['target_id' => $this->getFeatureByTitle('Wear a face covering to help reduce spread of COVID-19')],
+        ['target_id' => $this->getFeatureByTitle('Universal Credit')],
+        ['target_id' => $this->getFeatureByTitle('Coronavirus (COVID-19)')],
+      ],
+      'tag' => 1338,
+    ];
+
+    foreach ($fcl_content as $fcl) {
+      $node = Node::create([
+        'type' => 'featured_content_list',
+        'langcode' => 'en',
+        'moderation_state' => 'published',
+        'status' => 1,
+        'uid' => 1,
+        'title' => $fcl['title'],
+        'field_featured_content' => $fcl['features'],
+        'field_tags' => $fcl['tag'],
+      ]);
+
+      $node->save();
+      $this->getIo()->info("Created featured content list node with title '" . $fcl['title'] . "'");
+    }
+  }
+
+  /**
+   * Fetches the node id of a feature node from a given title.
+   *
+   * @param string $title
+   *   Feature node title.
+   * @return int
+   *   The node id.
+   */
+  private function getFeatureByTitle(string $title) {
+    foreach ($this->featureContent as $feature) {
+      if ($title === $feature['title']) {
+        return (int) $feature['nid'];
+      }
+    }
+
+    return 0;
+  }
+
+}
---
+++ migrate_nidirect_utils/src/Command/NidirectMigratePreFeatureNodesCommand.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Drupal\migrate_nidirect_utils\Command;
+
+use Drupal\Console\Core\Command\ContainerAwareCommand;
+// @codingStandardsIgnoreStart
+use Drupal\Console\Annotations\DrupalCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+// @codingStandardsIgnoreEnd
+/**
+ * Removes existing feature + featured_content_list nodes ahead of content import.
+ *
+ * @DrupalCommand (
+ *     extension="migrate_nidirect_utils",
+ *     extensionType="module"
+ * )
+ */
+class NidirectMigratePreFeatureNodesCommand extends ContainerAwareCommand {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function configure() {
+    $this->setName('nidirect:migrate:pre:feature_nodes')
+      ->setDescription("Pre migration setup: removes feature and FCL nodes");
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function execute(InputInterface $input, OutputInterface $output) {
+    $this->getIo()->info('Removing existing feature and featured_content_list nodes before migration.');
+    $this->task_remove_feature_fcl_nodes();
+    $this->getIo()->info('DONE!');
+  }
+
+  /**
+   * Purge the D8 site of all feature and featured content list nodes.
+   */
+  // phpcs:disable
+  protected function task_remove_feature_fcl_nodes() {
+  // phpcs:enable
+    $storage = \Drupal::entityTypeManager()->getStorage('node');
+    foreach (['featured_content_list', 'feature'] as $type) {
+      $entities = $storage->loadByProperties(['type' => $type]);
+      $storage->delete($entities);
+    }
+  }
+
+}
