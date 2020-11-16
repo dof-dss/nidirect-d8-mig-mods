f5e1b13aaa7e91fbfdd5efbed0753ae44dc91f40
omahm <hello@onemanandhismouse.com>
2020-11-16 14:17:27 +0000

    D8NID-961 ~ Migration prepare tasks

--- migrate_nidirect_utils/src/Commands/MigrationCommands.php
+++ migrate_nidirect_utils/src/Commands/MigrationCommands.php
@@ -4,6 +4,7 @@ namespace Drupal\migrate_nidirect_utils\Commands;
 
 use Drupal\Core\Database\Database;
 use Drush\Commands\DrushCommands;
+use Symfony\Component\Yaml\Yaml;
 
 /**
  * Drush pre migration command for NIDirect.
@@ -53,6 +54,225 @@ class MigrationCommands extends DrushCommands {
    * @aliases mig-stat
    */
   public function status() {
+  }
+
+  /**
+   * A simple migrate database query wrapper.
+   *
+   * @param string $query
+   *   SQL query to execute.
+   */
+  private function drupal7DatabaseQuery($query) {
+    $conn_query = $this->connMigrate->query($query);
+    return $conn_query->execute();
+  }
+
+  /**
+   *  --== Migration Prepare tasks ==--
+   */
+
+  /**
+   * Removes shortcuts from the default shortcut set.
+   *
+   * Remove to prevent errors during configuration import.
+   */
+// phpcs:disable
+  public function prepare_remove_default_shortcuts()
+  {
+    // phpcs:enable
+    // Remove the installed default admin shortcuts which trip up config
+    // sync import.
+    $query = \Drupal::entityTypeManager()->getStorage('shortcut')->getQuery();
+    $nids = $query->condition('shortcut_set', 'default')->execute();
+    $shortcuts = \Drupal::entityTypeManager()->getStorage("shortcut")->loadMultiple($nids);
+
+    if ($shortcuts) {
+      foreach ($shortcuts as $shortcut) {
+        $shortcut->delete();
+      }
+    }
+  }
+
+  /**
+   * Update the current site UUID.
+   *
+   * Update to use the config/sync site UUID or we won't
+   * be able to import configuration.
+   */
+// phpcs:disable
+  protected function prepare_update_site_uuid()
+  {
+    // phpcs:enable
+    global $config_directories;
+    $site_config = Yaml::parse(file_get_contents($config_directories['sync'] . '/system.site.yml'));
+
+    // Config imports will fail if the exported Site UUID doesn't match the
+    // current Site UUID.
+    if ($site_config) {
+      $site_uuid_sync = $site_config['uuid'];
+
+      $config = \Drupal::service('config.factory')->getEditable('system.site');
+      $site_uuid_curr = $config->get('uuid');
+
+      if ($site_uuid_sync != $site_uuid_curr) {
+        $config->set('uuid', $site_uuid_sync)->save();
+      }
+    }
+  }
+
+  /**
+   * Fix Health Condition revision with broken JSON contents.
+   */
+// phpcs:disable
+  protected function prepare_remove_broken_health_condition_revision()
+  {
+    // phpcs:enable
+    $this->drupal7DatabaseQuery("DELETE FROM node_revision WHERE nid=10748 AND vid=71939");
+  }
+
+  /**
+   * Fix Column 'title' cannot be null issues.
+   */
+// phpcs:disable
+  protected function prepare_null_titles()
+  {
+    // phpcs:enable
+    // Fix nodes.
+    $this->drupal7DatabaseQuery("UPDATE node SET node.title = '<none>' WHERE title = '' or title IS NULL");
+    // Fix node revisions.
+    $this->drupal7DatabaseQuery("UPDATE node_revision SET node_revision.title = '<none>' WHERE title = '' or title IS NULL");
+  }
+
+  /**
+   * Fix issue with zero status redirect imports to Drupal 8.
+   *
+   * Credit to Jaime Contreras.
+   */
+// phpcs:disable
+  protected function prepare_null_redirect_zero_state()
+  {
+    // phpcs:enable
+    $this->drupal7DatabaseQuery("UPDATE redirect SET status_code=301 WHERE status_code=0 OR status_code IS NULL");
+  }
+
+  /**
+   * Remove file usage data related to recipe images.
+   */
+// phpcs:disable
+  protected function prepare_remove_recipe_image_file_usage_data()
+  {
+    // phpcs:enable
+    $this->drupal7DatabaseQuery("DELETE file_usage FROM field_data_field_recipe_image INNER JOIN file_usage ON field_data_field_recipe_image.field_recipe_image_fid = file_usage.fid");
+  }
+
+  /**
+   * Remove file metadata data related to recipe images.
+   */
+// phpcs:disable
+  protected function prepare_remove_recipe_image_file_metadata()
+  {
+    // phpcs:enable
+    $this->drupal7DatabaseQuery("DELETE file_metadata FROM field_data_field_recipe_image INNER JOIN file_metadata ON field_data_field_recipe_image.field_recipe_image_fid = file_metadata.fid");
+  }
+
+  /**
+   * Remove managed file data related to recipe images.
+   */
+// phpcs:disable
+  protected function prepare_remove_recipe_image_managed_file_data()
+  {
+    // phpcs:enable
+    $this->drupal7DatabaseQuery("DELETE file_managed FROM field_data_field_recipe_image INNER JOIN file_managed ON field_data_field_recipe_image.field_recipe_image_fid = file_managed.fid");
+  }
+
+  /**
+   * Drop recipe image field tables.
+   */
+// phpcs:disable
+  protected function prepare_drop_recipe_image_field_tables()
+  {
+    // phpcs:enable
+    $this->drupal7DatabaseQuery("DROP TABLE IF EXISTS field_data_field_recipe_image,field_revision_field_recipe_image");
+  }
+
+  /**
+   * Remove recipe node and taxonomy path aliases.
+   */
+// phpcs:disable
+  protected function prepare_remove_recipe_url_aliases()
+  {
+    // phpcs:enable
+    $this->drupal7DatabaseQuery("DELETE FROM url_alias WHERE url_alias.alias LIKE 'recipes/%' OR url_alias.alias LIKE 'recipe-%'");
+  }
+
+  /**
+   * Remove recipe node metatags.
+   */
+// phpcs:disable
+  protected function prepare_remove_recipe_node_metatags()
+  {
+    // phpcs:enable
+    $this->drupal7DatabaseQuery("DELETE m FROM metatag m INNER JOIN node n ON n.nid = m.entity_id WHERE n.type = 'nidirect_recipe'");
+  }
+
+  /**
+   * Remove recipe node revisions.
+   */
+// phpcs:disable
+  protected function prepare_remove_recipe_node_revisions()
+  {
+    // phpcs:enable
+    $this->drupal7DatabaseQuery("DELETE r FROM node_revision r INNER JOIN node n ON n.nid = r.nid WHERE n.type = 'nidirect_recipe'");
+  }
+
+  /**
+   * Remove recipe nodes.
+   */
+// phpcs:disable
+  protected function prepare_remove_recipe_nodes()
+  {
+    // phpcs:enable
+    $this->drupal7DatabaseQuery("DELETE FROM node WHERE node.type = 'nidirect_recipe'");
+  }
+
+  /**
+   * Remove umbrella body node and taxonomy path aliases.
+   */
+// phpcs:disable
+  protected function prepare_remove_umbrella_body_url_aliases()
+  {
+    // phpcs:enable
+    $this->drupal7DatabaseQuery("DELETE FROM url_alias WHERE url_alias.alias LIKE 'accessni/%' OR url_alias.alias LIKE 'accessni-%'");
+  }
+
+  /**
+   * Remove umbrella body node metatags.
+   */
+// phpcs:disable
+  protected function prepare_remove_umbrella_body_node_metatags()
+  {
+    // phpcs:enable
+    $this->drupal7DatabaseQuery("DELETE m FROM metatag m INNER JOIN node n ON n.nid = m.entity_id WHERE n.type = 'nidirect_ub'");
+  }
+
+  /**
+   * Remove umbrella body node revisions.
+   */
+// phpcs:disable
+  protected function prepare_remove_umbrella_body_node_revisions()
+  {
+    // phpcs:enable
+    $this->drupal7DatabaseQuery("DELETE r FROM node_revision r INNER JOIN node n ON n.nid = r.nid WHERE n.type = 'nidirect_ub'");
+  }
+
+  /**
+   * Remove umbrella body nodes.
+   */
+// phpcs:disable
+  protected function prepare_remove_umbrella_body_nodes()
+  {
+    // phpcs:enable
+    $this->drupal7DatabaseQuery("DELETE FROM node WHERE node.type = 'nidirect_ub'");
   }
 
 
