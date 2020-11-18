04577b98bdab77edca96c7278c7ed5ddb0e649e5
omahm <hello@onemanandhismouse.com>
2020-11-18 10:15:01 +0000

    D8NID-961 ~ Switch from snake to camel case for prepare functions

--- migrate_nidirect_utils/src/Commands/MigrationCommands.php
+++ migrate_nidirect_utils/src/Commands/MigrationCommands.php
@@ -47,7 +47,7 @@ class MigrationCommands extends DrushCommands {
     $class_methods = get_class_methods($this);
 
     $migrate_tasks = array_filter($class_methods, function ($key) {
-      return substr($key, 0, 8) === "prepare_";
+      return substr($key, 0, 7) === "prepare";
     });
 
     if (($total_tasks = count($migrate_tasks)) > 1) {
@@ -93,14 +93,11 @@ class MigrationCommands extends DrushCommands {
    */
 
   /**
-   * Removes shortcuts from the default shortcut set.
+   * Removes default shortcut set.
    *
    * Remove to prevent errors during configuration import.
    */
-// phpcs:disable
-  public function prepare_remove_default_shortcuts()
-  {
-    // phpcs:enable
+  public function prepareRemoveDefaultShortcuts() {
     // Remove the installed default admin shortcuts which trip up config
     // sync import.
     $query = \Drupal::entityTypeManager()->getStorage('shortcut')->getQuery();
@@ -120,10 +117,7 @@ class MigrationCommands extends DrushCommands {
    * Update to use the config/sync site UUID or we won't
    * be able to import configuration.
    */
-// phpcs:disable
-  protected function prepare_update_site_uuid()
-  {
-    // phpcs:enable
+  protected function prepareUpdateSiteUuid() {
     global $config_directories;
     $site_config = Yaml::parse(file_get_contents($config_directories['sync'] . '/system.site.yml'));
 
@@ -144,20 +138,14 @@ class MigrationCommands extends DrushCommands {
   /**
    * Fix Health Condition revision with broken JSON contents.
    */
-// phpcs:disable
-  protected function prepare_remove_broken_health_condition_revision()
-  {
-    // phpcs:enable
+  protected function prepareRemoveBrokenHealthConditionRevision() {
     $this->drupal7DatabaseQuery("DELETE FROM node_revision WHERE nid=10748 AND vid=71939");
   }
 
   /**
    * Fix Column 'title' cannot be null issues.
    */
-// phpcs:disable
-  protected function prepare_null_titles()
-  {
-    // phpcs:enable
+  protected function prepareNullTitles() {
     // Fix nodes.
     $this->drupal7DatabaseQuery("UPDATE node SET node.title = '<none>' WHERE title = '' or title IS NULL");
     // Fix node revisions.
@@ -169,139 +157,98 @@ class MigrationCommands extends DrushCommands {
    *
    * Credit to Jaime Contreras.
    */
-// phpcs:disable
-  protected function prepare_null_redirect_zero_state()
-  {
-    // phpcs:enable
+  protected function prepareNullRedirectZeroState() {
     $this->drupal7DatabaseQuery("UPDATE redirect SET status_code=301 WHERE status_code=0 OR status_code IS NULL");
   }
 
   /**
    * Remove file usage data related to recipe images.
    */
-// phpcs:disable
-  protected function prepare_remove_recipe_image_file_usage_data()
-  {
-    // phpcs:enable
+  protected function prepareRemoveRecipeImageFileUsageData() {
     $this->drupal7DatabaseQuery("DELETE file_usage FROM field_data_field_recipe_image INNER JOIN file_usage ON field_data_field_recipe_image.field_recipe_image_fid = file_usage.fid");
   }
 
   /**
    * Remove file metadata data related to recipe images.
    */
-// phpcs:disable
-  protected function prepare_remove_recipe_image_file_metadata()
-  {
-    // phpcs:enable
+  protected function prepareRemoveRecipeImageFileMetadata() {
     $this->drupal7DatabaseQuery("DELETE file_metadata FROM field_data_field_recipe_image INNER JOIN file_metadata ON field_data_field_recipe_image.field_recipe_image_fid = file_metadata.fid");
   }
 
   /**
    * Remove managed file data related to recipe images.
    */
-// phpcs:disable
-  protected function prepare_remove_recipe_image_managed_file_data()
-  {
-    // phpcs:enable
+  protected function prepareRemoveRecipeImageManagedFileData() {
     $this->drupal7DatabaseQuery("DELETE file_managed FROM field_data_field_recipe_image INNER JOIN file_managed ON field_data_field_recipe_image.field_recipe_image_fid = file_managed.fid");
   }
 
   /**
    * Drop recipe image field tables.
    */
-// phpcs:disable
-  protected function prepare_drop_recipe_image_field_tables()
-  {
-    // phpcs:enable
+  protected function prepareDropRecipeImageFieldTables() {
     $this->drupal7DatabaseQuery("DROP TABLE IF EXISTS field_data_field_recipe_image,field_revision_field_recipe_image");
   }
 
   /**
    * Remove recipe node and taxonomy path aliases.
    */
-// phpcs:disable
-  protected function prepare_remove_recipe_url_aliases()
-  {
-    // phpcs:enable
+  protected function prepareRemoveRecipeUrlAliases() {
     $this->drupal7DatabaseQuery("DELETE FROM url_alias WHERE url_alias.alias LIKE 'recipes/%' OR url_alias.alias LIKE 'recipe-%'");
   }
 
   /**
    * Remove recipe node metatags.
    */
-// phpcs:disable
-  protected function prepare_remove_recipe_node_metatags()
-  {
-    // phpcs:enable
+  protected function prepareRemoveRecipeNodeMetatags() {
     $this->drupal7DatabaseQuery("DELETE m FROM metatag m INNER JOIN node n ON n.nid = m.entity_id WHERE n.type = 'nidirect_recipe'");
   }
 
   /**
    * Remove recipe node revisions.
    */
-// phpcs:disable
-  protected function prepare_remove_recipe_node_revisions()
-  {
-    // phpcs:enable
+  protected function prepareRemoveRecipeNodeRevisions() {
     $this->drupal7DatabaseQuery("DELETE r FROM node_revision r INNER JOIN node n ON n.nid = r.nid WHERE n.type = 'nidirect_recipe'");
   }
 
   /**
    * Remove recipe nodes.
    */
-// phpcs:disable
-  protected function prepare_remove_recipe_nodes()
-  {
-    // phpcs:enable
+  protected function prepareRemoveRecipeNodes() {
     $this->drupal7DatabaseQuery("DELETE FROM node WHERE node.type = 'nidirect_recipe'");
   }
 
   /**
    * Remove umbrella body node and taxonomy path aliases.
    */
-// phpcs:disable
-  protected function prepare_remove_umbrella_body_url_aliases()
-  {
-    // phpcs:enable
+  protected function prepareRemoveUmbrellaBodyUrlAliases() {
     $this->drupal7DatabaseQuery("DELETE FROM url_alias WHERE url_alias.alias LIKE 'accessni/%' OR url_alias.alias LIKE 'accessni-%'");
   }
 
   /**
    * Remove umbrella body node metatags.
    */
-// phpcs:disable
-  protected function prepare_remove_umbrella_body_node_metatags()
-  {
-    // phpcs:enable
+  protected function prepareRemoveUmbrellaBodyNodeMetatags() {
     $this->drupal7DatabaseQuery("DELETE m FROM metatag m INNER JOIN node n ON n.nid = m.entity_id WHERE n.type = 'nidirect_ub'");
   }
 
   /**
    * Remove umbrella body node revisions.
    */
-// phpcs:disable
-  protected function prepare_remove_umbrella_body_node_revisions()
-  {
-    // phpcs:enable
+  protected function prepareRemoveUmbrellaBodyNodeRevisions() {
     $this->drupal7DatabaseQuery("DELETE r FROM node_revision r INNER JOIN node n ON n.nid = r.nid WHERE n.type = 'nidirect_ub'");
   }
 
   /**
    * Remove umbrella body nodes.
    */
-// phpcs:disable
-  protected function prepare_remove_umbrella_body_nodes()
-  {
-    // phpcs:enable
+  protected function prepareRemoveUmbrellaBodyNodes() {
     $this->drupal7DatabaseQuery("DELETE FROM node WHERE node.type = 'nidirect_ub'");
   }
 
   /**
    * Fix issues with contact links.
    */
-  // phpcs:disable
-  protected function prepare_update_contact_links() {
-    // phpcs:enable
+  protected function prepareUpdateContactLinks() {
     // Update 'Central Appointments Unit' revision URL's to full path.
     $this->drupal7DatabaseQuery("UPDATE field_revision_field_contact_additional_info
     SET field_contact_additional_info_value = REGEXP_REPLACE(field_contact_additional_info_value, 'public-appointment-vacancies-64059.htm', 'https://www.nidirect.gov.uk/articles/public-appointment-vacancies')
@@ -329,15 +276,12 @@ class MigrationCommands extends DrushCommands {
   /**
    * Purge the D8 site of all feature and featured content list nodes.
    */
-  protected function prepare_remove_feature_fcl_nodes() {
-    // phpcs:enable
+  protected function prepareRemoveFeatureFclNodes() {
     $storage = \Drupal::entityTypeManager()->getStorage('node');
     foreach (['featured_content_list', 'feature'] as $type) {
       $entities = $storage->loadByProperties(['type' => $type]);
       $storage->delete($entities);
     }
   }
-
-
 
 }
